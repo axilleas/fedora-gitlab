5ea16f333d33f0537dc4ce8e1cd58c72132a449b
axilleas <axilleas@archlinux.gr>
2013-04-11 12:39:01 +0300

    Added generic script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Give execute permissions if not set
+chmod +x *sh *py
+
+# Find total gems packaged in Fedora
+sh rubysearch.sh
+
+# Remove unessecary file
+rm rubygems_fedora_raw
+
+# Find missing gems
+python2 gemfile.py
